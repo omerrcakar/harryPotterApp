9d43c56d8374fb8eae11e5c7dab0743b2bd27569
omer cakar <omerrccak@gmail.com>
2024-12-13 11:36:31 +0300

    harry Potter create App

---
+++ harryPotter/harryPotter/harryPotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  harryPotterApp.swift
+//  harryPotter
+//
+//  Created by ÖMER  on 13.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct harryPotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
